9c961b5aa9fa0d63361f1d595f790777f46d2bf1
Akbar <aumetov7@gmail.com>
2021-11-22 20:28:27 +0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Акбар Уметов on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

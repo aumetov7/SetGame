65a70df3a373355fd4d2f0d4af825d7764a78acb
Akbar <aumetov7@gmail.com>
2021-11-22 21:34:24 +0600

    ContentView small changes

--- SetGame/ContentView.swift
+++ SetGame/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var symbols = ["▲", "◼︎", "●", "△", "◻︎", "○",
+                          "▲▲", "◼︎◼︎", "●●", "△△", "◻︎◻︎", "○○",
+                          "▲▲▲", "◼︎◼︎◼︎", "●●●", "△△△", "◻︎◻︎◻︎", "○○○"]
+    let symbolsCount = 12
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Set").font(.largeTitle).foregroundColor(.blue)
+            
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                    ForEach(symbols[0..<symbolsCount], id:\.self) { symbol in
+                        CardView(content: symbol).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }.padding()
+            }
+            
+            Spacer()
+            
+            newGame
+            
+        }
+    }
+    
+    var newGame: some View {
+        Button(action: {
+            symbols.shuffle()
+        }, label: {
+            Image(systemName: "play.circle").font(.largeTitle)
+        })
+    }
+}
+
+struct CardView: View {
+    let content: String
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                
+                Text(content).font(font(in: geometry.size))
+            }.foregroundColor(.blue)
+        }
+    }
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.3
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+            .previewInterfaceOrientation(.portrait)
     }
 }
